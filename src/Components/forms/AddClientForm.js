73fad6b5ddf5838a43f4ca562437be7d3ccb40d3
tygedavis <tyge.davis@gmail.com>
2019-11-20 10:22:20 -0700

    Updated AddLoanForm and AddClientForm

--- src/Components/forms/AddClientForm.js
+++ src/Components/forms/AddClientForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
 import authAxios from '../../utils/authaxios';
 
 const AddClientForm = () => {
@@ -11,20 +12,80 @@ const AddClientForm = () => {
     harvest:  "",    // Optional
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = (values, { setStatus }) => {
+    payload.user_id = localStorage.getItem('userID');
     // authAxios call
-  }
-
-  const handleCancel = () => {
-    // history.push all-clients page || dashboard
-  }
+    authAxios.get('https://tiemendo.herokuapp.com/api/', payload)
+      .then(res => {
+        console.log(res.data)
+        setStatus(res.data) //TODO: Take out maybe??
+        values.history.push('./clients')
+      })
+      .catch( error => {
+        console.log("error", error.message)
+      })
+  };
 
   return (
     <div>
       <h2>Add a new Client</h2>
+
+      <Form className='form'>
+        <div className='flexDiv'>
+          <label/>Name
+          <Field
+             className='field'
+             name='name'
+             type='text'
+             placeholder='Name'
+           />
+          <ErrorMessage name="name" component='p' className='error'/>
+
+          <label/>Village
+          <Field
+            className='field'
+            name='village'
+            type='text'
+            placeholder='Village'
+          />
+          <ErrorMessage name="village" component='p' className='error'/>
+
+          <label/>Goal
+          <Field
+            className='field'
+            name='goal'
+            type='text'
+            placeholder='Due Date'
+          />
+
+          <label/>Harvest
+          <Field
+            className='field'
+            name='harvest'
+            type='text'
+            placeholder='Due Date'
+          />
+
+        </div>
+
+        <button type='submit' className='submitBtn'>Sign Up</button>
+      </Form>
     </div>
   )
 
 }
 
-export default AddClientForm;
+//Using Formik ----------------------------------------------------
+const FormikAddClientForm = withFormik({
+  mapPropsToValues({ name, village, goal, harvest, history }) {
+    return {
+      name: name || '',
+      village: village || '',
+      goal: goal || '',
+      harvest: harvest || '',
+      history: history
+    };
+  },
+})(AddClientForm);
+
+export default FormikAddClientForm;
